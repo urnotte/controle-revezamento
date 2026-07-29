3e483b7fa410f18870fee2378b3c1938923114c2
urnotte <maite.lacerda00@gmail.com>
2026-07-29 11:59:22 -0300

    Adiciona script inicial de criação do banco de dados

---
+++ sql/00-create-database.sql
@@ -0,0 +1,8 @@
+-- ==========================================================
+-- Projeto : Sistema de Controle de Revezamento
+-- Autor   : Maitê Lacerda
+-- Banco   : PostgreSQL
+-- Objetivo: Criação da estrutura física do banco de dados
+-- ==========================================================
+
+CREATE DATABASE controle_revezamento;
